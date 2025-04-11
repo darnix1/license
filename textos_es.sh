c4b3c05cea9e748aaf8a420d1c52a22c33f52cec
darnix1 <146903037+darnix1@users.noreply.github.com>
2025-04-11 09:05:18 -0600

    Update textos_es.sh

--- textos_es.sh
+++ textos_es.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+#TEXTOS MENU PRINCIPAL
+
+menu_op1="SSH OPENVPN"
+menu_op2="XRAY/VMESS"
+menu_op3="XRAY/VLESS"
+menu_op4="TROJAN"
+menu_op5="SERVICIOS"
+menu_op6="TELEGRAM BOT"
+menu_op7="NOTIBOT"
+menu_op8="TEMAS"
+menu_op9="UPDATE"
+menu_op10="SISTEMA"
+menu_op11="BACKUP"
+menu_op12="REINICIAR"
