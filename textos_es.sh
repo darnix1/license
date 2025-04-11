3a9bdeefe678143e6ac305ff8ef9760f9c05e666
darnix1 <146903037+darnix1@users.noreply.github.com>
2025-04-11 09:10:04 -0600

    Update textos_es.sh

--- textos_es.sh
+++ textos_es.sh
@@ -10,7 +10,12 @@ menu_op5="SERVICIOS"
 menu_op6="TELEGRAM BOT"
 menu_op7="NOTIBOT"
 menu_op8="TEMAS"
-menu_op9="UPDATE"
+menu_op9="ACTUALIZAR"
 menu_op10="SISTEMA"
 menu_op11="BACKUP"
 menu_op12="REINICIAR"
+
+
+# Input prompts
+ENTER_ID="Ingrese ID (obtener en @userinfobot)"
+ENTER_TOKEN="Ingrese Token del Bot (de @BotFather)"
